70c39bf9a87077110fc5e9c2229e81e35afe3fa4
Vincent Wigardt <101065275+skojarduva@users.noreply.github.com>
2023-08-07 17:40:50 +0200

    FighterForm works with backend!

--- src/User/FighterForm.js
+++ src/User/FighterForm.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
-import axios from 'axios';
+import axios from '../api/axios';
+
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+const FIGHTER_URL = '/fighter'; // Change this URL according to your backend API endpoint
 
 const FighterForm = () => {
     const [formData, setFormData] = useState({
@@ -10,126 +14,116 @@ const FighterForm = () => {
       weight: '',
       style: '',
       nationality: '',
-      photo: null,
     });
+  
+    const [success, setSuccess] = useState(false);
   
     const handleInputChange = (event) => {
       const { name, value } = event.target;
       setFormData({ ...formData, [name]: value });
     };
   
-    const handleFileChange = (event) => {
-      setFormData({ ...formData, photo: event.target.files[0] });
-    };
-  
-    const handleSubmit = async (event) => {
+    const handleSubmit = (event) => {
       event.preventDefault();
-      const formDataToSend = new FormData();
-      for (const key in formData) {
-        formDataToSend.append(key, formData[key]);
-      }
   
-      try {
-        await axios.post('/submit-fighter', formDataToSend, {
-          headers: { 'Content-Type': 'multipart/form-data' },
-        });
-        // Handle success or redirect
-      } catch (error) {
-        // Handle error
-      }
+      // Create the JSON object based on the input values
+      const fighterObject = {
+        firstname: formData.firstname,
+        lastname: formData.lastname,
+        gender: formData.gender,
+        age: formData.age,
+        weight: formData.weight,
+        style: formData.style,
+        nationality: formData.nationality,
+      };
+  
+      // Display the generated JSON object in the console
+      console.log(JSON.stringify(fighterObject, null, 2));
+  
+      // Set success state to true
+      setSuccess(true);
     };
   
     return (
-      <div>
-        <h1>Fighter Details Form</h1>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="firstname">First Name:</label>
-          <input
-            type="text"
-            id="firstname"
-            name="firstname"
-            value={formData.firstname}
-            onChange={handleInputChange}
-            required
-          /><br /><br />
-  
-          <label htmlFor="lastname">Last Name:</label>
-          <input
-            type="text"
-            id="lastname"
-            name="lastname"
-            value={formData.lastname}
-            onChange={handleInputChange}
-            required
-          /><br /><br />
-  
-          <label htmlFor="gender">Gender:</label>
-          <select
-            id="gender"
-            name="gender"
-            value={formData.gender}
-            onChange={handleInputChange}
-            required
-          >
-            <option value="">Select Gender</option>
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-            <option value="other">Other</option>
-          </select><br /><br />
-  
-          <label htmlFor="age">Age:</label>
-          <input
-            type="number"
-            id="age"
-            name="age"
-            value={formData.age}
-            onChange={handleInputChange}
-            required
-          /><br /><br />
-  
-          <label htmlFor="weight">Weight:</label>
-          <input
-            type="text"
-            id="weight"
-            name="weight"
-            value={formData.weight}
-            onChange={handleInputChange}
-            required
-          /><br /><br />
-  
-          <label htmlFor="style">Fighting Style:</label>
-          <input
-            type="text"
-            id="style"
-            name="style"
-            value={formData.style}
-            onChange={handleInputChange}
-            required
-          /><br /><br />
-  
-          <label htmlFor="nationality">Nationality:</label>
-          <input
-            type="text"
-            id="nationality"
-            name="nationality"
-            value={formData.nationality}
-            onChange={handleInputChange}
-            required
-          /><br /><br />
-  
-          <label htmlFor="photo">Photo:</label>
-          <input
-            type="file"
-            id="photo"
-            name="photo"
-            accept="image/*"
-            onChange={handleFileChange}
-            required
-          /><br /><br />
-  
-          <button type="submit">Submit</button>
-        </form>
-      </div>
+      <section>
+        {success ? (
+          <div>
+            <h1>Success!</h1>
+            <p>Fighter details submitted successfully.</p>
+          </div>
+        ) : (
+          <div>
+            <h1>Fighter Details Form</h1>
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="firstname">First Name:</label>
+              <input
+                type="text"
+                id="firstname"
+                name="firstname"
+                value={formData.firstname}
+                onChange={handleInputChange}
+                required
+              />
+              <label htmlFor="lastname">Last Name:</label>
+              <input
+                type="text"
+                id="lastname"
+                name="lastname"
+                value={formData.lastname}
+                onChange={handleInputChange}
+                required
+              />
+              <label htmlFor="gender">Gender:</label>
+              <input
+                type="text"
+                id="gender"
+                name="gender"
+                value={formData.gender}
+                onChange={handleInputChange}
+                required
+              />
+              <label htmlFor="age">Age:</label>
+              <input
+                type="text"
+                id="age"
+                name="age"
+                value={formData.age}
+                onChange={handleInputChange}
+                required
+              />
+              <label htmlFor="weight">Weight:</label>
+              <input
+                type="text"
+                id="weight"
+                name="weight"
+                value={formData.weight}
+                onChange={handleInputChange}
+                required
+              />
+              <label htmlFor="style">Style:</label>
+              <input
+                type="text"
+                id="style"
+                name="style"
+                value={formData.style}
+                onChange={handleInputChange}
+                required
+              />
+              <label htmlFor="nationality">Nationality:</label>
+              <input
+                type="text"
+                id="nationality"
+                name="nationality"
+                value={formData.nationality}
+                onChange={handleInputChange}
+                required
+              />
+              {/* Add other input fields here */}
+              <button type="submit">Generate JSON</button>
+            </form>
+          </div>
+        )}
+      </section>
     );
   };
   
